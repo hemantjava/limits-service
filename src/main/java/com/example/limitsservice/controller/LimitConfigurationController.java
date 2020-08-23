a7cad79b9bafe2418b6b2f3756eae0cfe973ca38
hemantjava <hemantjava90@gmail.com>
2020-08-23 17:52:25 +0530

    zipkin

--- src/main/java/com/example/limitsservice/controller/LimitConfigurationController.java
+++ src/main/java/com/example/limitsservice/controller/LimitConfigurationController.java
@@ -2,8 +2,11 @@ package com.example.limitsservice.controller;
 
 import com.example.limitsservice.config.Configuration;
 import com.example.limitsservice.config.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +19,24 @@ public class LimitConfigurationController {
 
     //localhost:9090/limits
     @GetMapping("limits")
+
     public LimitConfiguration retrieveLimitConfig ( ) {
         LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMax(), configuration.getMin());
         log.info(limitConfiguration);
         return limitConfiguration;
+    }
+
+    //localhost:9090/fault-tolerance
+    @GetMapping("/fault-tolerance")
+    @HystrixCommand(fallbackMethod = "fallbackConfiguration")
+    public LimitConfiguration getFaultTolerance(){
+        throw new RuntimeException("Not Available");
+
+    }
+
+    public LimitConfiguration fallbackConfiguration(){ //graceful behaviour
+        log.info("fallbackConfiguration");
+        return new LimitConfiguration(1, 10);
+
     }
 }
